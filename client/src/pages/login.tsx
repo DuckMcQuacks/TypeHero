9253e05956c77bce6a5eaba1affe06200f177532
DuckMcQuacks <menus.dominik@gmail.com>
2023-02-23 16:14:11 +0100

    Login

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,9 +1,38 @@
+import Link from "next/link";
+import {useState} from "react";
+
 export default function login()
 {
+    interface login {
+        username: string,
+        password: string
+    }
+    const [formValues, setFormValues] = useState<login>({
+        username:"",
+        password:""
+    });
+    function changeHandler(e: React.ChangeEvent<HTMLInputElement>){
+        setFormValues(prev=>{
+            return {...prev, [e.target.name]: e.target.value}            
+        })
+    }
+    function handleSubmit(e:React.FormEvent<HTMLFormElement>){
+        e.preventDefault();
+    }
     return(
         <div className="loginPage">
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <h1>LOGIN</h1>
+                <hr />
+                <div>
+                <input type="text" name="username" id="username" value={formValues.username} onChange={changeHandler} placeholder="Username"/>
+                <input type="password" name="password" id="password" value={formValues.password} onChange={changeHandler} placeholder="Password"/>
+                <button>Login</button>
+                <div style={{background:"transparent"}}>
+                Don't have an account?
+                <Link href={"/signup"} style={{background:"transparent", color:"#FFAE00", padding:"10px", display:" inline-block"}}>Sign up</Link>
+                </div>
+                </div>
             </form>
         </div>
     )
