f169333d85594d70b22a1638c2278c1c21731e98
DuckMcQuacks <menus.dominik@gmail.com>
2023-02-27 17:53:21 +0100

    Form checking

--- client/src/pages/api/hello.ts
+++ client/src/pages/api/hello.ts
@@ -1,13 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-
+const { MongoClient } = require("mongodb");
+const uri = "mongodb+srv://typehero:zLeBwhPldgiNpmWQ@typehero.yqdawhq.mongodb.net/?retryWrites=true&w=majority";
 type Data = {
   name: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler(req: NextApiRequest,res: NextApiResponse<Data>){
+  const client = new MongoClient(uri);
+  const database = client.db("")
 }
--- client/src/pages/signup.tsx
+++ client/src/pages/signup.tsx
@@ -1,15 +1,17 @@
 import Link from "next/link";
 import {useState} from "react";
 
-export default function login()
+export default function signup()
 {
-    interface login {
+    interface signup {
         username: string,
-        password: string
+        password: string,
+        passwordConfirm: string
     }
-    const [formValues, setFormValues] = useState<login>({
+    const [formValues, setFormValues] = useState<signup>({
         username:"",
-        password:""
+        password:"",
+        passwordConfirm: "",
     });
     function changeHandler(e: React.ChangeEvent<HTMLInputElement>){
         setFormValues(prev=>{
@@ -18,27 +20,33 @@ export default function login()
     }
     function handleSubmit(e:React.FormEvent<HTMLFormElement>){
         e.preventDefault();
+        if(formValues.password != formValues.passwordConfirm){
+            alert("Passwords do not match!");
+            return 3
+        }
         if(formValues.username.includes(" ")){
             alert("Username cannot contain white spaces!")
-            return 1;
+            return 1
         }
         if(formValues.username.replace(/[^a-zA-Z0-9 ]/g, '')!=formValues.username){
             alert("Username cannot contain special characters!")
-            return 2;
+            return 2
         }
+        
     }
     return(
         <div className="loginPage">
             <form onSubmit={handleSubmit}>
-                <h1>LOGIN</h1>
+                <h1>REGISTER</h1>
                 <hr />
                 <div>
                 <input required type="text" name="username" id="username" value={formValues.username} onChange={changeHandler} placeholder="Username"/>
                 <input required type="password" name="password" id="password" value={formValues.password} onChange={changeHandler} placeholder="Password"/>
-                <button>Login</button>
+                <input required type="password" name="passwordConfirm" id="passwordConfirm" value={formValues.passwordConfirm} onChange={changeHandler} placeholder="Password again"/>
+                <button>Sign up</button>
                 <div style={{background:"transparent"}}>
-                Don't have an account?
-                <Link href={"/signup"} style={{background:"transparent", color:"#FFAE00", padding:"10px", display:" inline-block"}}>Sign up</Link>
+                Have an account?
+                <Link href={"/login"} style={{background:"transparent", color:"#FFAE00", padding:"10px", display:" inline-block"}}>Login</Link>
                 </div>
                 </div>
             </form>
